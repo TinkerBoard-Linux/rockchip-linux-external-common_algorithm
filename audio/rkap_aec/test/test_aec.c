4927ccc297edef15711600d78be2a499e4cbfb7c
Yu YongZhen <yuyz@rock-chips.com>
2020-08-21 19:57:09 +0800

    audio: rkap: update to version RKAP_AEC_V1.01
    Signed-off-by: Yu YongZhen <yuyz@rock-chips.com>
    Change-Id: Ie3da41cd54718c5d52f05f76fcb3ceb11e602c20

--- audio/rkap_aec/test/test_aec.c
+++ audio/rkap_aec/test/test_aec.c
@@ -29,52 +29,91 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "AP_AEC.h"
 
 int main(int argc, char **argv)
 {
-    RKAP_Handle *ap_aec;
+    RKAP_Handle *ap_aec_tx = NULL, * ap_aec_rx = NULL;
     RKAP_AEC_State state;
-    FILE *fp_near = NULL, *fp_far = NULL, *fp_out = NULL;
+    FILE *fp_near = NULL, *fp_far = NULL, *fp_out_tx = NULL, *fp_out_rx = NULL;
+    int swFs;
+    int swFrameLen;
     void *vParaData;
     short *swBufferIn, *swBufferRef, *swBufferOut;
-    int swFs = 8000;
-    int swFrameLen = swFs / 50;
     int dataLen = 0;
+    char *out_rx_file = NULL;
 
-    if (argc != 5)
+#ifdef FOR_VS_DEBUG
+    /* For Debug  */
+    argc = 6;
+    argv[1] = (char *)"../../../test_file/txin_noise.pcm";
+    argv[2] = (char *)"../../../test_file/rxin.pcm";
+    argv[3] = (char *)"../../../test_file/out_1.pcm";
+    argv[4] = (char *)"../../../para/para_ser_8k.bin";
+    argv[5] = (char *)"8000";
+#endif
+    if (argc != 6)
     {
-        fprintf(stderr, "%s is not 5\n", argv[0]);
+        AEC_DumpVersion();
+        fprintf(stderr, "Usage: ./test_aec <near.pcm> <far.pcm> <out.pcm> <para.bin> <rate 8000 or 16000>\n");
+        fprintf(stderr, "For example:\n");
+        fprintf(stderr, "  ./test_aec rec.pcm ref.pcm out_aec.pcm RKAP_AecPara.bin 8000\n");
         exit(1);
     }
     fp_near = fopen(argv[1], "rb");
     if (fp_near == NULL)
     {
-        fprintf(stderr, "%s fp_near fopen failed\n", argv[0]);
+        fprintf(stderr, "%s fp_near fopen failed\n", argv[1]);
         exit(1);
     }
     fp_far = fopen(argv[2], "rb");
     if (fp_far == NULL)
     {
-        fprintf(stderr, "%s fp_far fopen failed\n", argv[0]);
+        fprintf(stderr, "%s fp_far fopen failed\n", argv[2]);
         exit(1);
     }
-    fp_out = fopen(argv[3], "wb");
-    if (fp_near == NULL)
+    fp_out_tx = fopen(argv[3], "wb");
+    if (fp_out_tx == NULL)
     {
-        fprintf(stderr, "%s fp_out fopen failed\n", argv[0]);
+        fprintf(stderr, "%s fp_out_tx fopen failed\n", argv[3]);
         exit(1);
     }
+    out_rx_file = (char *)calloc(1, strlen(argv[3]) + 4);
+    sprintf(out_rx_file, "%s_rx", argv[3]);
+    fp_out_rx = fopen(out_rx_file, "wb");
+    if (fp_out_rx == NULL)
+    {
+        fprintf(stderr, "%s fp_out_rx fopen failed\n", out_rx_file);
+        exit(1);
+    }
+
+    state.pathPara = argv[4];
+
+    swFs = atoi(argv[5]);
+    if (swFs != 8000 && swFs != 16000)
+    {
+        fprintf(stderr, "Not supported sample rate: %d\n", swFs);
+        exit(1);
+    }
+    swFrameLen  = swFs / 50;
 
     state.swSampleRate = swFs;
     state.swFrameLen = swFrameLen;
-    state.pathPara = argv[4];
 
-    ap_aec = AEC_Init(&state);
-    if (ap_aec == NULL)
+    /* tx init */
+    ap_aec_tx = AEC_Init(&state, AEC_TX_TYPE);
+    if (ap_aec_tx == NULL)
     {
-        fprintf(stderr, "ap_aec_Init failed\n");
+        fprintf(stderr, "ap_aec_tx_Init failed\n");
+        exit(1);
+    }
+    /* rx init */
+    ap_aec_rx = AEC_Init(&state, AEC_RX_TYPE);
+    if (ap_aec_rx == NULL)
+    {
+        fprintf(stderr, "ap_aec_rx_Init failed\n");
         exit(1);
     }
 
@@ -86,11 +125,14 @@ int main(int argc, char **argv)
     {
         dataLen = fread(swBufferIn, sizeof(short), swFrameLen, fp_near);
         dataLen = fread(swBufferRef, sizeof(short), swFrameLen, fp_far);
-        AEC_Process(ap_aec, swBufferIn, swBufferRef, swBufferOut);
-        fwrite(swBufferOut, sizeof(short), swFrameLen, fp_out);
+        AEC_Process(ap_aec_rx, swBufferIn, NULL, swBufferOut);
+        fwrite(swBufferOut, sizeof(short), swFrameLen, fp_out_rx);
+        AEC_Process(ap_aec_tx, swBufferIn, swBufferRef, swBufferOut);
+        fwrite(swBufferOut, sizeof(short), swFrameLen, fp_out_tx);
     }
 
-    AEC_Destroy(ap_aec);
+    AEC_Destroy(ap_aec_tx);
+    AEC_Destroy(ap_aec_rx);
     free(swBufferRef);
     free(swBufferIn);
     free(swBufferOut);
@@ -99,9 +141,12 @@ int main(int argc, char **argv)
         fclose(fp_far);
     if (fp_near)
         fclose(fp_near);
-    if (fp_out)
-        fclose(fp_out);
-
+    if (fp_out_tx)
+        fclose(fp_out_tx);
+    if (fp_out_rx)
+        fclose(fp_out_rx);
+    if (out_rx_file)
+        free(out_rx_file);
     printf("done...\n");
 
     return 0;
